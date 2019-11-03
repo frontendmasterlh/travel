739411d83269e46772540e179719f120113b2063
leislie <296454971@qq.com>
2019-11-03 16:45:52 +0800

    dinish

---
+++ src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+import store from './store'
+import 'babel-polyfill'
+
+Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
+fastClick.attach(document.body)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
